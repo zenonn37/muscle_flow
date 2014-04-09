de680a4e469a08629eb515bafe7da66ee6e09c98
chris crowd <zent29@gmail.com>
2014-04-09 14:26:42 -0400

    fixes

---
+++ app/scripts/services/serviceCtrl.js
@@ -0,0 +1,10 @@
+/**
+ * Created by pure coder on 4/9/2014.
+ */
+angular.module('muscleFlow_App')
+
+          .controller('serviceCtrl',['$scope',function($scope){
+
+          $scope.header = "Services";
+
+    }]);
